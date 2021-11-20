1e2f8d7a908ee6af9df8a2f5b564da931a5e19dd
Rachid Lamouri <rachidlamouri@gmail.com>
2021-11-20 17:58:24 -0500

    Convert parser logs to an error
    It's not ideal, but no one is really supporting antlr4ts

--- src/parse.ts
+++ src/parse.ts
@@ -1,9 +1,34 @@
 import { CommonTokenStream } from 'antlr4ts';
 import { InputContext, SchemeParser } from './language/compiled/SchemeParser';
 
+type LogSuppressor = {
+  originalLogger: typeof console.error;
+  logs: any[];
+  disableLogs: Function;
+  enableLogs: Function;
+}
+
+const errorLogSuppressor: LogSuppressor = {
+  originalLogger: console.error,
+  logs: [],
+  disableLogs() {
+    this.logs = [];
+    console.error = (...args) => this.logs.push(args);
+  },
+  enableLogs() {
+    console.error = this.originalLogger;
+    return this.logs;
+  },
+};
+
 export const parse = (tokenStream: CommonTokenStream): InputContext => {
   const parser = new SchemeParser(tokenStream);
-  SchemeParser
+  errorLogSuppressor.disableLogs();
   const rootAstNode = parser.input();
+  const errors = errorLogSuppressor.enableLogs();
+  if (errors.length > 0) {
+    throw Error(`Unable to parse input\n` + errors.map((args: any[]) => args.join('')).join('\n'));
+  }
+
   return rootAstNode;
 }
