bf52f5f19f134faf9b3f8ddd55e0c20bdcdec727
Rachid Lamouri <rachidlamouri@gmail.com>
2021-11-25 17:15:42 -0500

    Add test descriptions and colors

--- src/run.test.ts
+++ src/run.test.ts
@@ -1,4 +1,5 @@
 import { expect } from 'earljs';
+import chalk from 'chalk';
 import { run } from './run';
 
 type MochaConfig = {
@@ -6,34 +7,34 @@ type MochaConfig = {
 }
 
 type RunConfig =
-  [code: string, expectedOutput: string]
-  | [code: string, expectedOutput: string, config: MochaConfig];
+  [description: string, code: string, expectedOutput: string]
+  | [description: string, code: string, expectedOutput: string, config: MochaConfig];
 
 describe('run', () => {
   const tests: RunConfig[] = [
-    ["'atom", 'atom'],
-    ["'turkey", 'turkey'],
-    ["'1492", '1492'],
-    ["'u", 'u'],
-    ["'*abc$", '*abc$'],
-    ["'(atom)", "(atom)"],
-    ["'(atom turkey or)", "(atom turkey or)"],
-    ["'((atom))", "((atom))"],
-    ["'((atom turkey))", "((atom turkey))"],
-    ["'((atom turkey) or)", "((atom turkey) or)"],
-    ["'(or (atom turkey))", "(or (atom turkey))"],
-    ["'()", "()"],
-    ["'(() () () ())", "(() () () ())"],
-    ['(car (a))', 'a'],
-    ['(car (a b))', 'a'],
-    ['(car (a b c))', 'a'],
-    ['(car ((a b c) x y z))', '(a b c)'],
-    ['(car (((hotdogs)) (and) (pickle) relish))', '((hotdogs))'],
+    ['string', "'atom", 'atom'],
+    ['string', "'turkey", 'turkey'],
+    ['integer', "'1492", '1492'],
+    ['single character', "'u", 'u'],
+    ['special characters', "'*abc$", '*abc$'],
+    ['list', "'(atom)", "(atom)"],
+    ['list of atoms', "'(atom turkey or)", "(atom turkey or)"],
+    ['nested list', "'((atom))", "((atom))"],
+    ['nested list of atoms', "'((atom turkey))", "((atom turkey))"],
+    ['mixed nested s-expressions', "'((atom turkey) or)", "((atom turkey) or)"],
+    ['mixed nested s-expressions', "'(or (atom turkey))", "(or (atom turkey))"],
+    ['empty list', "'()", "()"],
+    ['nested empty lists', "'(() () () ())", "(() () () ())"],
+    ['car of list length 1', '(car (a))', 'a'],
+    ['car of list length 2', '(car (a b))', 'a'],
+    ['car of list length 3', '(car (a b c))', 'a'],
+    ['car of lst starting with list', '(car ((a b c) x y z))', '(a b c)'],
+    ['car of list with mixed s-expression', '(car (((hotdogs)) (and) (pickle) relish))', '((hotdogs))'],
   ];
 
-  tests.forEach(([code, expectedOutput, config = { isOnly: false }]) => {
+  tests.forEach(([description, code, expectedOutput, config = { isOnly: false }]) => {
     const method = config.isOnly ? it.only : it;
-    method(`${code} -> ${expectedOutput}`, () => {
+    method(`${chalk.cyan(description)}: ${chalk.blue(code)} -> ${chalk.yellow(expectedOutput)}`, () => {
       expect(run(code)).toEqual(expectedOutput);
     });
   });
