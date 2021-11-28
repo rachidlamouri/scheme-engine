2d3a5e85ed634015a4de17a142998db16cc795b7
Rachid Lamouri <rachidlamouri@gmail.com>
2021-11-28 09:42:17 -0500

    Separate tests by concept

--- src/run.test.ts
+++ src/run.test.ts
@@ -15,8 +15,34 @@ type RunConfig =
 const t = true;
 const f = false;
 
+const runTest = ([isFromBook, description, code, expectedOutput, config = { isOnly: false }]: RunConfig) => {
+  const method = config.isOnly ? it.only : it;
+  const isErrorTest = typeof expectedOutput === 'object';
+  const outputDescription = isErrorTest
+    ? chalk.red(expectedOutput.error)
+    : chalk.yellow(expectedOutput)
+
+  const prefix = isFromBook ? '*' : ' ';
+
+  method(`${prefix} ${chalk.cyan(description)}: ${chalk.blue(code)} -> ${outputDescription}`, () => {
+    if (isErrorTest) {
+      expect(() => run(code)).toThrow(expectedOutput.error);
+    } else {
+      expect(run(code)).toEqual(expectedOutput);
+    }
+  });
+};
+
+const runSuite = (suiteName: string, tests: RunConfig[]) => {
+  describe(suiteName, () => {
+    tests.forEach(runTest);
+  })
+};
+
+process.stdout.write(`${chalk.gray('*')}: denotes test from The Little Schemer`);
+
 describe('run', () => {
-  const tests: RunConfig[] = [
+  runSuite('literals', [
     [t, 'string', "'atom", 'atom'],
     [t, 'string', "'turkey", 'turkey'],
     [t, 'integer', "'1492", '1492'],
@@ -25,54 +51,40 @@ describe('run', () => {
     [t, 'special characters', "'*abc$", '*abc$'],
     [t, 'list', "'(atom)", "(atom)"],
     [t, 'list of atoms', "'(atom turkey or)", "(atom turkey or)"],
-    [f,'nested list', "'((atom))", "((atom))"],
+    [f, 'nested list', "'((atom))", "((atom))"],
     [t, 'nested list of atoms', "'((atom turkey))", "((atom turkey))"],
     [f, 'mixed nested s-expressions', "'((atom turkey) or)", "((atom turkey) or)"],
     [f, 'mixed nested s-expressions', "'(or (atom turkey))", "(or (atom turkey))"],
     [t, 'empty list', "'()", "()"],
     [t, 'nested empty lists', "'(() () () ())", "(() () () ())"],
-    [f, 'car of list length 1', "(car '(a))", 'a'],
-    [f, 'car of list length 2', "(car '(a b))", 'a'],
-    [t, 'car of list length 3', "(car '(a b c))", 'a'],
-    [t, 'car of list starting with list', "(car '((a b c) x y z))", '(a b c)'],
-    [t, 'car of string atom', "(car 'hotdog)", { error: 'Cannot get the car of atom "hotdog"' }],
-    [t, 'car of empty list', "(car '())", { error: 'Cannot get the car of an empty list' }],
-    [t, 'car of list with mixed s-expression', "(car '(((hotdogs)) (and) (pickle) relish))", '((hotdogs))'],
+  ]);
+
+  runSuite('car', [
+    [f, 'list of length 1', "(car '(a))", 'a'],
+    [f, 'list of  length 2', "(car '(a b))", 'a'],
+    [t, 'list of  length 3', "(car '(a b c))", 'a'],
+    [t, 'list starting with list', "(car '((a b c) x y z))", '(a b c)'],
+    [t, 'string atom', "(car 'hotdog)", { error: 'Cannot get the car of atom "hotdog"' }],
+    [t, 'empty list', "(car '())", { error: 'Cannot get the car of an empty list' }],
+    [t, 'list with mixed s-expression', "(car '(((hotdogs)) (and) (pickle) relish))", '((hotdogs))'],
     [f, 'nested car expressions', "(car (car '((a))))", 'a'],
     [f, 'nested car expressions', "(car (car (car '(((a))))))", 'a'],
     [f, 'nested car expressions', "(car (car '( ((hotdogs)) ((and)) ) ))", '(hotdogs)'],
-    [f, 'car of integer atom', "(car 1234)", { error: 'Cannot get the car of atom "1234"' }],
+    [f, 'integer atom', "(car 1234)", { error: 'Cannot get the car of atom "1234"' }],
     [f, 'invalid nested car', "(car (car '(a)))", { error: 'Cannot get the car of returned atom "a"' }],
-    [t, 'cdr', "(cdr '(a b c))", '(b c)'],
-    [t, 'cdr', "(cdr '((a b c) x y z))", '(x y z)'],
-    [t, 'cdr', "(cdr '(hamburger))", '()'],
-    [t, 'cdr', "(cdr '((x) t r))", '(t r)'],
-    [t, 'cdr', "(cdr 'hotdogs)", { error: 'Cannot get the cdr of atom "hotdogs"'}],
-    [t, 'cdr', "(cdr '())", { error: 'Cannot get the cdr of an empty list'}],
+  ]);
+
+  runSuite('cdr', [
+    [t, 'list', "(cdr '(a b c))", '(b c)'],
+    [t, 'car of list is list', "(cdr '((a b c) x y z))", '(x y z)'],
+    [t, 'list with one atom', "(cdr '(hamburger))", '()'],
+    [t, 'car of list is list', "(cdr '((x) t r))", '(t r)'],
+    [t, 'atom', "(cdr 'hotdogs)", { error: 'Cannot get the cdr of atom "hotdogs"'}],
+    [t, 'empty list', "(cdr '())", { error: 'Cannot get the cdr of an empty list'}],
     [f, 'nested cdr', "(cdr (cdr '(a b c)))", '(c)'],
     [f, 'nested cdr error', "(cdr (cdr '(a)))", { error: 'Cannot get the cdr of the returned empty list'}],
     [t, 'nested car and cdr', "(car (cdr '((b) (x y) ((c))) ))", '(x y)'],
     [t, 'nested cdr', "(cdr (cdr '((b) (x y) ((c))) ))", '(((c)))'],
     [t, 'nested cdr and car', "(cdr (car '(a (b (c)) d) ))", { error: 'Cannot get the cdr of returned atom "a"' }],
-  ];
-
-  process.stdout.write(`${chalk.gray('*')}: denotes test from The Little Schemer`);
-
-  tests.forEach(([isFromBook, description, code, expectedOutput, config = { isOnly: false }]) => {
-    const method = config.isOnly ? it.only : it;
-    const isErrorTest = typeof expectedOutput === 'object';
-    const outputDescription = isErrorTest
-      ? chalk.red(expectedOutput.error)
-      : chalk.yellow(expectedOutput)
-
-    const prefix = isFromBook ? '*' : ' ';
-
-    method(`${prefix} ${chalk.cyan(description)}: ${chalk.blue(code)} -> ${outputDescription}`, () => {
-      if (isErrorTest) {
-        expect(() => run(code)).toThrow(expectedOutput.error);
-      } else {
-        expect(run(code)).toEqual(expectedOutput);
-      }
-    });
-  });
+  ]);
 });
