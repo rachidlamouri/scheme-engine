146defcb783b87566bf73d67c140f3f7352c30b2
Rachid Lamouri <rachidlamouri@gmail.com>
2021-12-29 22:29:15 -0500

    Improve interpreter debugging experience

--- src/interpreter/interpret.ts
+++ src/interpreter/interpret.ts
@@ -3,6 +3,10 @@ import { refineInputContext } from './input';
 
 export const interpret = (rootAstNode: InputContext): string => {
   return refineInputContext(rootAstNode)
-    .map((evaluable) => evaluable.evaluate().toString())
+    .map((evaluable) => {
+      const result = evaluable.evaluate();
+      const serializedResult = result.toString();
+      return serializedResult;
+    })
     .join('\n');
 }
