2ee632986329b37bf99721c20106447315236493
Rachid Lamouri <rachidlamouri@gmail.com>
2021-12-30 15:01:06 -0500

    Remove circular import dependency hack

--- src/interpreter/evaluableClass.ts
+++ src/interpreter/evaluableClass.ts
@@ -1,3 +1,5 @@
+// This class has to live outside of ./evaluable to prevent circular import dependencies
+// Almost everything is an Evaluable but "refineEvaluableContext" needs to import everything
 export abstract class Evaluable {
   abstract evaluate(...args: any[]): Evaluable;
 }
--- src/interpreter/index.ts
+++ src/interpreter/index.ts
@@ -1,8 +1,3 @@
-// TODO: figure out why we need to do this (btw it wasn't necessary one commit ago):
-// import modules that have circular dependencies first
-import './symbolicExpression';
-import './evaluable';
-
 export { Evaluable } from './evaluableClass';
 export { executionContext } from './executionContext';
 export { refineInputContext as refineRootAstNode } from './input';
