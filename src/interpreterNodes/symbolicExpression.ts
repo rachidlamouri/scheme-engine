78a2b6b45966751b8fdb2cacdc2c6e7ac1741ff4
Rachid Lamouri <rachidlamouri@gmail.com>
2022-01-01 14:23:08 -0500

    Remove unused function

--- src/interpreterNodes/symbolicExpression.ts
+++ src/interpreterNodes/symbolicExpression.ts
@@ -3,5 +3,3 @@ import { Atom } from './atom';
 import { Primitive } from './utils';
 
 export type SymbolicExpression = List | Atom<Primitive>;
-
-export const isSymbolicExpression = (arg: any): arg is SymbolicExpression => arg instanceof List || arg instanceof Atom;
